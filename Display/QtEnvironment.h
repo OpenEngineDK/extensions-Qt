d4caccfeacb9efb0ad9827d4ea82d33c7ea4e164
cpvc <cpvc@openengine.dk>
2009-04-10 17:52:55 +0000

    added qt key event handlers

--- Display/QtEnvironment.h
+++ Display/QtEnvironment.h
@@ -85,14 +85,127 @@ public:
             joystick = new QtJoystick();
         }
 
-        void keyPressEvent(QKeyEvent *e) {
+        Key ConvertKey(Qt::Key key) {
+            // numbers
+            if (key>=Qt::Key_0 && key<=Qt::Key_9) {
+                unsigned int value = (key - Qt::Key_0);
+                //logger.info << "int:" << value << logger.end;
+                return Key(value + KEY_0);
+            }
+            // chars
+            if (key>=Qt::Key_A && key<=Qt::Key_Z) {
+                unsigned int value = (key - Qt::Key_A);
+                //logger.info << "char:" << value << logger.end;
+                return Key(value + KEY_a);
+            }
+
+            // function keys
+            if (key>=Qt::Key_F1 && key<=Qt::Key_F15) {
+                unsigned int value = (key - Qt::Key_F1);
+                //logger.info << "func key:" << value << logger.end;
+                return Key(value + KEY_F1);
+            }
+
+            if (key == Qt::Key_Escape) return KEY_ESCAPE;
+            if (key == Qt::Key_Tab) return KEY_TAB;
+            if (key == Qt::Key_Backspace) return KEY_BACKSPACE;
+            if (key == Qt::Key_Return) return KEY_RETURN;
+            if (key == Qt::Key_Insert) return KEY_INSERT;
+            if (key == Qt::Key_Delete) return KEY_DELETE;
+            if (key == Qt::Key_Pause) return KEY_PAUSE;
+            if (key == Qt::Key_Print) return KEY_PRINT;
+            if (key == Qt::Key_SysReq) return KEY_SYSREQ;
+            if (key == Qt::Key_Home) return KEY_HOME;
+            if (key == Qt::Key_End) return KEY_END;
+            if (key == Qt::Key_Left) return KEY_LEFT;
+            if (key == Qt::Key_Right) return KEY_RIGHT;
+            if (key == Qt::Key_Down) return KEY_DOWN;
+            if (key == Qt::Key_Up) return KEY_UP;
+            if (key == Qt::Key_PageUp) return KEY_PAGEUP;
+            if (key == Qt::Key_PageDown) return KEY_PAGEDOWN;
+            if (key == Qt::Key_Shift) return KEY_LSHIFT;
+            if (key == Qt::Key_Control) return KEY_LCTRL;
+            if (key == Qt::Key_Meta) return KEY_LMETA;
+            if (key == Qt::Key_Alt) return KEY_LALT;
+            if (key == Qt::Key_AltGr) return KEY_RALT;
+            if (key == Qt::Key_CapsLock) return KEY_CAPSLOCK;
+            if (key == Qt::Key_NumLock) return KEY_NUMLOCK;
+            if (key == Qt::Key_ScrollLock) return KEY_SCROLLOCK;
+            if (key == Qt::Key_Plus) return KEY_PLUS;
+            if (key == Qt::Key_Comma) return KEY_COMMA;
+            if (key == Qt::Key_Minus) return KEY_MINUS;
+            if (key == Qt::Key_Period) return KEY_PERIOD;
+            if (key == Qt::Key_Slash) return KEY_SLASH;
+            if (key == Qt::Key_Colon) return KEY_COLON;
+            if (key == Qt::Key_Semicolon) return KEY_SEMICOLON;
+            if (key == Qt::Key_Space) return KEY_SPACE;
+
+            logger.warning << "unknown key in qt handler:" << key << logger.end;
+            return KEY_UNKNOWN;
+        }
+
+        KeyMod ConvertModKey(Qt::KeyboardModifiers mkey) {
+            KeyMod rtn = KEY_MOD_NONE;
+            if (!mkey)
+                return rtn;
+            if (mkey & Qt::ShiftModifier) {
+                rtn = KeyMod(rtn |KEY_LSHIFT);
+                rtn = KeyMod(rtn |KEY_RSHIFT);
+                mkey = Qt::KeyboardModifiers(mkey & ~Qt::ShiftModifier);
+                //logger.info << "modifier key: shift down" << logger.end;
+            }
+            if (mkey & Qt::ControlModifier) {
+                rtn = KeyMod(rtn |KEY_LCTRL);
+                rtn = KeyMod(rtn |KEY_RCTRL);
+                mkey = Qt::KeyboardModifiers(mkey & ~Qt::ControlModifier);
+                //logger.info << "modifier key: ctrl down" << logger.end;
+            }
+            if (mkey & Qt::AltModifier) {
+                rtn = KeyMod(rtn |KEY_LALT);
+                rtn = KeyMod(rtn |KEY_RALT);
+                mkey = Qt::KeyboardModifiers(mkey & ~Qt::AltModifier);
+                //logger.info << "modifier key: alt down" << logger.end;
+            }
+            if (mkey & Qt::MetaModifier) {
+                rtn = KeyMod(rtn |KEY_LMETA);
+                rtn = KeyMod(rtn |KEY_RMETA);
+                mkey = Qt::KeyboardModifiers(mkey & ~Qt::MetaModifier);
+                //logger.info << "modifier key: meta down" << logger.end;
+            }
+
+            if (mkey!=0)
+                logger.warning << "MODIFIER KEY, not handled in Qt key handler"
+                               << logger.end;
+            return rtn;
+        }
+
+        void keyPressEvent(QKeyEvent* e) {
+            //logger.info << "press event" << logger.end;
             KeyboardEventArg arg;
-            //todo: set sym, mod, type
+            arg.type = EVENT_PRESS;
+
+            Qt::Key key = (Qt::Key)e->key();
+            arg.sym = ConvertKey(key);
+
+            Qt::KeyboardModifiers keym = (Qt::KeyboardModifiers)e->modifiers();
+            arg.mod = ConvertModKey(keym);
+
             keyboard->ke.Notify( arg );
-            logger.info << "key press event" << logger.end;
         }
         
-        //void keyReleaseEvent(QKeyEvent *e) {}
+        void keyReleaseEvent(QKeyEvent *e) {
+            //logger.info << "release event" << logger.end;
+            KeyboardEventArg arg;
+            arg.type = EVENT_RELEASE;
+
+            Qt::Key key = (Qt::Key)e->key();
+            arg.sym = ConvertKey(key);
+
+            Qt::KeyboardModifiers keym = (Qt::KeyboardModifiers)e->modifiers();
+            arg.mod = ConvertModKey(keym);
+
+            keyboard->ke.Notify( arg );
+        }
 
         IMouse* GetMouse() {
             return mouse;
